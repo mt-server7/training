d65d9d4b79f3ecaec0b2a0a311fd547952480456
Михаил Т <m.t-server7@yandex.ru>
2023-05-26 10:47:50 +0300

    Добавлены задачи с 6 по 9

--- chapter_1_simple_programs/1/class.cpp
+++ chapter_1_simple_programs/1/class.cpp
@@ -156,22 +156,121 @@ class Task_5
 
 class Task_6
 {
+    private:
+        const double km_in_mil = 1.6093;
+        double kilometer;
+        double Output(double kilometer)
+        {
+            std::cout<<"Введите значение:";
+            std::cin>>kilometer;
+            std::cout<<kilometer / km_in_mil<<std::endl;
+        }
 
+    public:
+        Task_6(double kilo)
+        {
+            Set_num(kilo);
+            Output(kilo);
+        }
+
+        ~Task_6(){}
+
+        void Set_num(double kilo)
+        {
+            kilometer = kilo;
+        }
 };
 
 class Task_7
 {
+    private:
+        const double km_in_mil = 1.6093;
+        const double metr_in_ft = 3.2808;
+        int kilometer, metr, dmil, dft;
+        double Output(int kilometer, int metr)
+        {
+            std::cout<<"Введите значение километров и метров:";
+            std::cin>>kilometer>>metr;
+            double dist_km = (double)kilometer + (double)metr / 1000;
+            dmil = (double) dist_km / km_in_mil;
+            dft = (dist_km - dmil) + metr_in_ft * (double)metr;
+            std::cout<<"Дистанция в милях и футах: "<< dmil<<" mill"<<" "<<dft<<" ft"<<std::endl;
+        }
 
+    public:
+        Task_7(int kilo, int mt)
+        {
+            Set_num(kilo, mt);
+            Output(kilo, mt);
+        }
+
+        ~Task_7(){}
+
+        void Set_num(int kilo, int mt)
+        {
+            kilometer = kilo;
+            metr = mt;
+        }
 };
 
 class Task_8
 {
+    private:
+        const double sh_in_mt = 2.16;
+        double metr;
+        double Output(double metr)
+        {
+            std::cout<<"Введите значение:";
+            std::cin>>metr;
+            std::cout<<metr * sh_in_mt<<std::endl;
+        }
 
+    public:
+        Task_8(double mtr)
+        {
+            Set_num(mtr);
+            Output(mtr);
+        }
+
+        ~Task_8(){}
+
+        void Set_num(double mtr)
+        {
+            metr = mtr;
+        }
 };
 
 class Task_9
 {
+    private:
+        const double sh_in_mt = 2.16;
+        const int ar_in_sh = 3;
+        int metr, sm, ar, sh;
+        double Output(int sh, int ar)
+        {
+            std::cout<<"Введите значение сажаней и аршин:";
+            std::cin>>sh>>ar;
+            double dist_sh = (double)sh + (double)ar * ar_in_sh;
+            double dmetr = (double)sh * sh_in_mt;
+            metr = (int)dmetr;
+            sm = (int)(dmetr - metr) * 100;
+            std::cout<<"Дистанция в метрах и сантиметра: "<< metr<<" м"<<" "<<sm<<"см"<<std::endl;
+        }
 
+    public:
+        Task_9(int sh1, int ar1)
+        {
+            Set_num(sh1, ar1);
+            Output(sh1, ar1);
+        }
+
+        ~Task_9(){}
+
+        void Set_num(int sh1, int ar1)
+        {
+            sh = sh1;
+            ar = ar1;
+        }
 };
 
 class Task_10
